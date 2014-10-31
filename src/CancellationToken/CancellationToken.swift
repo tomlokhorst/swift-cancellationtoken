d73680d3b72ff80760d71ae66ef074f03a5b8adc
Tom Lokhorst <tom@lokhorst.eu>
2014-10-31 14:46:07 +0100

    Some initial code

---
+++ src/CancellationToken/CancellationToken.swift
@@ -0,0 +1,80 @@
+//
+//  CancellationToken.swift
+//  CancellationToken
+//
+//  Created by Tom Lokhorst on 2014-10-31.
+//
+//
+
+import Foundation
+
+/**
+A `CancellationToken` indicates if cancellation of "something" was requested.
+Can be passed around and checked by whatever wants to be cancellable.
+
+To create a cancellation token, use `CancellationTokenSource`.
+*/
+public class CancellationToken {
+  public private(set) var isCancellationRequested = false
+  private var handlers : [Void -> Void] = []
+
+  internal init() {
+  }
+
+  internal func tryCancel() -> Bool {
+    if !isCancellationRequested {
+      isCancellationRequested = true
+      executeHandlers()
+
+      return true
+    }
+
+    return false
+  }
+
+  public func register(handler: Void -> Void) {
+    if isCancellationRequested {
+      handler()
+    }
+    else {
+      handlers.append(handler)
+    }
+  }
+
+  private func executeHandlers() {
+    // Call all previously scheduled handlers
+    for handler in handlers {
+      handler()
+    }
+
+    // Cleanup
+    handlers = []
+  }
+}
+
+/**
+A `CancellationTokenSource` is used to create a `CancellationToken`.
+The created token can be set to "cancellation requested" using the `cancel()` method.
+*/
+public class CancellationTokenSource {
+  public let token = CancellationToken()
+
+  public init() {
+  }
+
+  public func cancel() {
+    token.tryCancel()
+  }
+
+  public func cancel(when: dispatch_time_t) {
+    // On main queue, for now
+    dispatch_after(when, dispatch_get_main_queue()) {
+      self.token.tryCancel()
+      return
+    }
+  }
+
+  public func cancel(seconds: Double) {
+    cancel(dispatch_time(DISPATCH_TIME_NOW, Int64(seconds * Double(NSEC_PER_SEC))))
+  }
+}
---
+++ src/CancellationTokenTests/CancellationTokenTests.swift
@@ -0,0 +1,104 @@
+//
+//  CancellationTokenTests.swift
+//  CancellationTokenTests
+//
+//  Created by Tom Lokhorst on 2014-10-31.
+//
+//
+
+import UIKit
+import XCTest
+import CancellationToken
+
+class CancellationTokenTests: XCTestCase {
+
+  func testDefault() {
+    let source = CancellationTokenSource()
+    let token = source.token
+
+    XCTAssertFalse(token.isCancellationRequested, "Default should be false")
+  }
+
+  func testCancel() {
+    let source = CancellationTokenSource()
+    let token = source.token
+
+    source.cancel()
+
+    XCTAssert(token.isCancellationRequested, "Token should be cancelled")
+  }
+
+  func testDelayedCancel() {
+    let expectation = expectationWithDescription("Cancel not registered")
+
+    let source = CancellationTokenSource()
+    let token = source.token
+
+    delay(0.01) {
+      source.cancel()
+    }
+
+    delay(0.02) {
+      if token.isCancellationRequested {
+        expectation.fulfill()
+      }
+    }
+
+    waitForExpectationsWithTimeout(0.03, handler: nil)
+  }
+
+  func testCancelWithDelay() {
+    let expectation = expectationWithDescription("Cancel not registered")
+
+    let source = CancellationTokenSource()
+    let token = source.token
+
+    source.cancel(0.01)
+    XCTAssertFalse(token.isCancellationRequested, "Cancel should still be false")
+
+    delay(0.02) {
+      if token.isCancellationRequested {
+        expectation.fulfill()
+      }
+    }
+
+    waitForExpectationsWithTimeout(0.03, handler: nil)
+  }
+
+  func testRegisterBefore() {
+    let expectation = expectationWithDescription("Cancel not registered")
+
+    let source = CancellationTokenSource()
+    let token = source.token
+
+    token.register {
+      expectation.fulfill()
+    }
+
+    source.cancel(0.01)
+
+    waitForExpectationsWithTimeout(0.02, handler: nil)
+  }
+
+  func testRegisterAfter() {
+    let expectation = expectationWithDescription("Cancel not registered")
+
+    let source = CancellationTokenSource()
+    let token = source.token
+
+    source.cancel()
+
+    token.register {
+      expectation.fulfill()
+    }
+
+    waitForExpectationsWithTimeout(0.01, handler: nil)
+  }
+}
+
+func delay(seconds: NSTimeInterval, block: dispatch_block_t!) {
+  let when = dispatch_time(DISPATCH_TIME_NOW, Int64(seconds * Double(NSEC_PER_SEC)))
+  let queue = dispatch_get_main_queue()
+
+  dispatch_after(when, queue, block)
+}
